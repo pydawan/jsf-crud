624e4b813687420a923a07df93803b05d917f1aa
Thiago Monteiro <pydawan@gmail.com>
2017-09-17 20:15:44 -0300

    Adicionados novos métodos de conveniência para testar se um registro é ativo ou inativo.

--- src/main/java/br/org/jsf/crud/RegistrationStatus.java
+++ src/main/java/br/org/jsf/crud/RegistrationStatus.java
@@ -1,19 +1,23 @@
 package br.org.jsf.crud;
 
+import static br.org.verify.Verify.isNotNull;
+
 /**
  * <p>
  * Representa as possíveis situações cadastrais
  * de um registro no sistema.
  * </p>
+ * 
  * @author thiago-amm
  * @version v1.0.0 10/09/2017
+ * @version v1.0.1 17/09/2017
  * @since v1.0.0
  */
 public enum RegistrationStatus {
    
    ACTIVE("Ativo", "ATIVO"), 
    INACTIVE("Inativo", "INATIVO");
-   
+      
    private final String label;
    private final String value;
    
@@ -36,6 +40,52 @@ public enum RegistrationStatus {
    
    public String value() {
       return value;
+   }
+   
+   public static boolean isActive(RegistrationStatus registrationStatus) {
+      if (isNotNull(registrationStatus) && ACTIVE.equals(registrationStatus)) {
+         return true;
+      }
+      return false;
+   }
+   
+   public static boolean active(RegistrationStatus registrationStatus) {
+      return isActive(registrationStatus);
+   }
+   
+   public static boolean isActive(String registrationStatus) {
+      RegistrationStatus active = RegistrationStatus.valueOf(registrationStatus);
+      if (isNotNull(active) && active.equals(ACTIVE)) {
+         return true;
+      }
+      return false;
+   }
+   
+   public static boolean active(String registrationStatus) {
+      return isActive(registrationStatus);
+   }
+   
+   public static boolean isInactive(RegistrationStatus registrationStatus) {
+      if (isNotNull(registrationStatus) && INACTIVE.equals(registrationStatus)) {
+         return true;
+      }
+      return false;
+   }
+   
+   public static boolean inactive(RegistrationStatus registrationStatus) {
+      return isInactive(registrationStatus);
+   }
+   
+   public static boolean isInactive(String registrationStatus) {
+      RegistrationStatus inactive = RegistrationStatus.valueOf(registrationStatus);
+      if (isNotNull(inactive) && inactive.equals(INACTIVE)) {
+         return true;
+      }
+      return false;
+   }
+   
+   public static boolean inactive(String registrationStatus) {
+      return isInactive(registrationStatus);
    }
    
    @Override
--- src/main/java/br/org/jsf/crud/SituacaoCadastro.java
+++ src/main/java/br/org/jsf/crud/SituacaoCadastro.java
@@ -1,19 +1,23 @@
 package br.org.jsf.crud;
 
+import static br.org.verify.Verify.isNotNull;
+
 /**
  * <p>
  * Representa as possíveis situações cadastrais
  * de um registro no sistema.
  * </p>
+ * 
  * @author thiago-amm
  * @version v1.0.0 10/09/2017
+ * @version v1.0.0 17/09/2017
  * @since v1.0.0
  */
 public enum SituacaoCadastro {
    
    ATIVO("Ativo", "ATIVO"), 
    INATIVO("Inativo", "INATIVO");
-   
+      
    private final String nome;
    private final String valor;
    
@@ -36,6 +40,52 @@ public enum SituacaoCadastro {
    
    public String valor() {
       return getValor();
+   }
+   
+   public static boolean isAtivo(SituacaoCadastro situacaoCadastro) {
+      if (isNotNull(situacaoCadastro) && ATIVO.equals(situacaoCadastro)) {
+         return true;
+      }
+      return false;
+   }
+   
+   public static boolean ativo(SituacaoCadastro situacaoCadastro) {
+      return isAtivo(situacaoCadastro);
+   }
+   
+   public static boolean isAtivo(String situacaoCadastro) {
+      SituacaoCadastro ativo = SituacaoCadastro.valueOf(situacaoCadastro);
+      if (isNotNull(ativo) && ativo.equals(ATIVO)) {
+         return true;
+      }
+      return false;
+   }
+   
+   public static boolean ativo(String situacaoCadastro) {
+      return isAtivo(situacaoCadastro);
+   }
+   
+   public static boolean isInativo(SituacaoCadastro situacaoCadastro) {
+      if (isNotNull(situacaoCadastro) && INATIVO.equals(situacaoCadastro)) {
+         return true;
+      }
+      return false;
+   }
+   
+   public static boolean inativo(SituacaoCadastro situacaoCadastro) {
+      return isInativo(situacaoCadastro);
+   }
+   
+   public static boolean isInativo(String situacaoCadastro) {
+      SituacaoCadastro inativo = SituacaoCadastro.valueOf(situacaoCadastro);
+      if (isNotNull(inativo) && inativo.equals(INATIVO)) {
+         return true;
+      }
+      return false;
+   }
+   
+   public static boolean inativo(String situacaoCadastro) {
+      return isInativo(situacaoCadastro);
    }
    
    @Override
